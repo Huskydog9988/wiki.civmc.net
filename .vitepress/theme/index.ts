aee63568017c31e662baf7e2716233cd7ce734f8
Huskydog9988 <39809509+Huskydog9988@users.noreply.github.com>
2025-07-23 13:27:09 -0400

    redirect to new site

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,16 +1,22 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
-import Theme from 'vitepress/theme'
-import './style.css'
+import { h } from "vue";
+import Theme from "vitepress/theme";
+import "./style.css";
+import { EnhanceAppContext } from "vitepress";
+
+const newWiki = "https://civmc.net";
 
 export default {
   extends: Theme,
   Layout: () => {
     return h(Theme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
+    });
   },
-  enhanceApp({ app, router, siteData }) {
-    // ...
-  }
-}
+  enhanceApp({ app, router, siteData }: EnhanceAppContext) {
+    if (typeof window !== "undefined") {
+      // Only run in browser
+      window.location.replace(newWiki);
+    }
+  },
+};
---
+++ README.md
@@ -0,0 +1,4 @@
+# CivMC Wiki
+
+> [!WARNING]
+> The CivMC Wiki has moved to the [Civ monorepo](https://github.com/CivMC/Civ/tree/main/docs). This repository is no longer maintained.
